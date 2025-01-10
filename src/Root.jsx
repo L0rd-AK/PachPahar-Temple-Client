de0af8f49d18935caee112e7a7327176e1e9a004
l0rd-AK <aka883207@gmail.com>
2025-01-11 04:52:20 +0600

    tostify added

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-
-const Root=()=>{
-    return(
-        <>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </>
-    );
-}
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Root = () => {
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 export default Root;
