e58f0d929c79dd1fc7155ba22b5c75983025103c
l0rd-AK <aka883207@gmail.com>
2025-01-15 04:22:04 +0600

    signup with just email added

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -43,7 +43,7 @@ const SignIn = () => {
               Sign in to your account
             </h1>
             <namem className="space-y-4 md:space-y-6" action="#">
-              <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                   <label
                     name="email"
@@ -58,6 +58,7 @@ const SignIn = () => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required
+                    {...register("email")}
                   />
                 </div>
                 <div>
@@ -74,6 +75,7 @@ const SignIn = () => {
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
+                    {...register("password")}
                   />
                 </div>
                 <div className="flex items-center justify-between">
