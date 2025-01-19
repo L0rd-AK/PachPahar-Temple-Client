62f1113060da38c97c6c2da61dd8146cde5c70b4
l0rd-AK <aka883207@gmail.com>
2025-01-19 12:36:37 +0600

    analytics not working

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import Routes from "./components/Routes/Routes.jsx";
 import AuthProvider from "../src/components/AuthProvider/AuthProvider.jsx";
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={Routes} />
+      <Analytics />
     </AuthProvider>
   </React.StrictMode>
 );
