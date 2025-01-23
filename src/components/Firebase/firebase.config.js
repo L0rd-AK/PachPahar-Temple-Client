71a0bc729fb91a6c9e16a6c1933b8be7c5f29d9e
l0rd-AK <aka883207@gmail.com>
2025-01-24 00:07:06 +0600

    firebase credentials hidded

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -4,18 +4,13 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey:import.meta.env.VITE_apiKey,
-  // authDomain:import.meta.env.VITE_authDomain,
-  // projectId:import.meta.env.VITE_projectId,
-  // storageBucket:import.meta.env.VITE_storageBucket,
-  // messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  // appId:import.meta.env.VITE_appId,
-  apiKey: "AIzaSyBl_ZaVGapN4cLFtmLp14bY5UyJlih4Bt4",
-  authDomain: "pachpahar-ba4c8.firebaseapp.com",
-  projectId: "pachpahar-ba4c8",
-  storageBucket: "pachpahar-ba4c8.firebasestorage.app",
-  messagingSenderId: "276805977240",
-  appId: "1:276805977240:web:86b9428367159f6ea6fced"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+  
 };
 
 // Initialize Firebase
